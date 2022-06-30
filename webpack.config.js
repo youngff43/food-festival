344683a4e846347f15e88faffe27852bb0466982
youngff43 <youngff43@gmail.com>
2022-06-30 15:51:56 -0400

    updated files

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,26 @@
 const path = require("path");
 const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js'
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+      },
+      output: {
+        filename: "main.bundle.js",
+        path: __dirname + "/dist",
       },
     plugins: [
         new webpack.ProvidePlugin({
             $: "jquery",
             jQuery: "jquery"
           }),
+          new BundleAnalyzerPlugin({
+            analyzerMode: "static", // the report outputs to an HTML file in the dist folder
+          })
     ],
     mode: 'development'
 };
